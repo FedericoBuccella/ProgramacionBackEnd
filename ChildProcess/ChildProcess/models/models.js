956296e46fe64e450f74acf0e1941039875d4961
Federico Buccella <91340763+FedericoBuccella@users.noreply.github.com>
2022-09-24 00:37:01 -0300

    Delete ChildProcess/ChildProcess directory

--- ChildProcess/ChildProcess/models/models.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Usuario',{
-    username: String,
-    password: String,
-    email: String,
-    firstName: String,
-    lastName: String
-});
